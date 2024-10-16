77a3b9eb1a360ee00bb79d83bbe5e9ba4794f8b2
Barnouin <barnouin@eurecom.fr>
2024-10-16 20:22:09 +0000

    still can't compite speed

--- script/part2_module.c
+++ script/part2_module.c
@@ -3,6 +3,7 @@
 #include <linux/gpio.h>
 #include <linux/interrupt.h>
 #include <linux/timer.h>
+#include <linux/delay.h>
 
 /* Meta Information */
 MODULE_LICENSE("GPL");
@@ -18,27 +19,29 @@ MODULE_DESCRIPTION("A simple LKM for a gpio interrupt");
 unsigned int irq_number;
 
 /* Declare two timers for periodic interrupts */
-static struct timer_list timer_8ms;
+// static struct timer_list timer_8ms;
 static struct timer_list timer_1s;
-static struct timer_list timer_450us;
-static struct timer_list timer_4us;
+// static struct timer_list timer_400us;
+// static struct timer_list timer_4us;
 
 /* Function prototypes for timer callbacks */
-void timer_8ms_callback(struct timer_list *timer);
+// void timer_8ms_callback(struct timer_list *timer);
 void timer_1s_callback(struct timer_list *timer);
-void timer_400us_callback(struct timer_list *timer);
-void timer_4us_callback(struct timer_list *timer);
+// void timer_400us_callback(struct timer_list *timer);
+// void timer_4us_callback(struct timer_list *timer);
 
 // All our variables
 
-bool last_state_A = 0;
-bool last_state_B = 0;
+bool last_state_A;
+bool last_state_B;
 bool curr_state_A;
 bool curr_state_B;
 
 int speed = 0;
+int actual_value = 0;
 int count_pulses = 0;
 int count_pwm = 0;
+int count_max_pwm = 100;
 
 // Controller
 // PI Controller parameters
@@ -47,10 +50,10 @@ double Ti = 1.0;  // Integral time
 double T = 0.008; // Sample time (8ms)
 double sum_error = 0; // Integral of the error
 double max_speed = 3000;
-double reference_value = 2000; // Target speed
-double actual_value = 0; 
+double reference_value = 2000; // Target speed 
 double u = 0;
 
+static int pin18_state = 0;
 
 /**
  * @brief Interrupt service routine is called, when interrupt is triggered
@@ -58,42 +61,59 @@ double u = 0;
 static irqreturn_t gpio_irq_handler(int irq, void *dev_id)
 {
     // printk("gpio_irq: Interrupt was triggered and ISR was called!\n");
+    // sleep for 50 us
+    // usleep_range(50, 55);
+    udelay(50);
+    // pin18_state = !pin18_state;
+    // gpio_set_value(GPIO_18, pin18_state);
 
-    if (last_state_A == last_state_B) {
-        curr_state_A = gpio_get_value(GPIO_17);
-        if (curr_state_A != last_state_A) {
-            count_pulses++;
-        } else {
-            curr_state_B = gpio_get_value(GPIO_19);
-        }
-    } else {
-        curr_state_A = gpio_get_value(GPIO_17);
-        if (curr_state_A != last_state_A) {
-            count_pulses--;
-        } else {
-            curr_state_B = gpio_get_value(GPIO_19);
-        }
-    }
+    // int gpio_value = gpio_get_value(GPIO_17);
+    // printk("GPIO 17 value: %d\n", gpio_value);
 
-    last_state_A = curr_state_A;
-    last_state_B = curr_state_B;
+    // int gpio17_value;
+    // int gpio19_value;
     
-    return IRQ_HANDLED;
+    // if (last_state_A == last_state_B) {
+    //     gpio17_value = gpio_get_value(GPIO_17);
+    //     curr_state_A = (gpio17_value == 0);
+    //     if (curr_state_A != last_state_A) {
+    //         count_pulses++;
+    //         // gpio_set_value(GPIO_18, curr_state_A);
+    //     } else {
+    //         gpio19_value = gpio_get_value(GPIO_19);
+    //         curr_state_B = (gpio19_value == 0);
+    //     }
+    // } else {
+    //     gpio17_value = gpio_get_value(GPIO_17);
+    //     curr_state_A = (gpio17_value == 0);
+    //     if (curr_state_A != last_state_A) {
+    //         count_pulses--;
+    //         // gpio_set_value(GPIO_18, curr_state_A);
+    //     } else {
+    //         gpio19_value = gpio_get_value(GPIO_19);
+    //         curr_state_B = (gpio19_value == 0);
+    //     }
+    // }
+
+    // last_state_A = curr_state_A;
+    // last_state_B = curr_state_B;
+
+ return IRQ_HANDLED;
 }
 
 /**
  * @brief Callback function for the 8ms timer
  */
 
-void timer_8ms_callback(struct timer_list *timer) // Pi controller logic is here
-{
+// void timer_8ms_callback(struct timer_list *timer) // Pi controller logic is here
+// {
 
-    actual_value = speed;
+//     actual_value = speed;
 
-    // gives speed to user space via input buffer
-    // reads from user space count_max_pwm 
+//     // gives speed to user space via input buffer
+//     // reads from user space count_max_pwm 
 
-    mod_timer(&timer_8ms, jiffies + msecs_to_jiffies(8));
+//     mod_timer(&timer_8ms, jiffies + msecs_to_jiffies(8));
 
     // double error = reference_value - actual_value;
     // sum_error_priv += error * T_priv;
@@ -107,25 +127,25 @@ void timer_8ms_callback(struct timer_list *timer) // Pi controller logic is here
     // if (u < 1){ u = 1;} //limiting the lower bound of the duty cycle
 
     // Restart the timer for another 8 ms
-}
+// }
 
-void timer_400us_callback(struct timer_list *timer)
-{
-    gpio_set_value(GPIO_13, 0);
-    count_pwm = 0;
-    mod_timer(&timer_400us, jiffies + usecs_to_jiffies(400));
-}
+// void timer_400us_callback(struct timer_list *timer)
+// {
+//     gpio_set_value(GPIO_13, 0);
+//     count_pwm = 0;
+//     mod_timer(&timer_400us, jiffies + usecs_to_jiffies(400));
+// }
 
-void timer_4us_callback(struct timer_list *timer)
-{
-    if (count_pwm != count_max_pwm){
-        count_pwm++;
-    }
-    else{
-        gpio_set_value(GPIO_13, 1);
-    }
-    mod_timer(&timer_4us, jiffies + usecs_to_jiffies(4));
-}
+// void timer_4us_callback(struct timer_list *timer)
+// {
+//     if (count_pwm != count_max_pwm){
+//         count_pwm++;
+//     }
+//     else{
+//         gpio_set_value(GPIO_13, 1);
+//     }
+//     mod_timer(&timer_4us, jiffies + usecs_to_jiffies(4));
+// }
 
 /**
  * @brief Callback function for the 1 second timer
@@ -136,8 +156,8 @@ void timer_1s_callback(struct timer_list *timer)
     //printk("Timer 1s: Interrupt triggered\n");
 
     /* Restart the timer for another 1 second */
-    printk("Pulses: %d\n", count_pulses);
     speed = count_pulses;
+    printk("Pulses: %d\n", speed);
     count_pulses = 0;
     mod_timer(&timer_1s, jiffies + msecs_to_jiffies(1000));
 }
@@ -218,24 +238,27 @@ static int __init ModuleInit(void)
         return -1;
     }
 
+    last_state_A = (gpio_get_value(GPIO_17) == 0);
+    last_state_B = (gpio_get_value(GPIO_19) == 0);
+
     printk("Done!\n");
     printk("GPIO 17 is mapped to IRQ Nr.: %d\n", irq_number);
 
-    /* Initialize and start the 8 ms timer */
-    timer_setup(&timer_8ms, timer_8ms_callback, 0);
-    mod_timer(&timer_8ms, jiffies + msecs_to_jiffies(8));
+    // /* Initialize and start the 8 ms timer */
+    // timer_setup(&timer_8ms, timer_8ms_callback, 0);
+    // mod_timer(&timer_8ms, jiffies + msecs_to_jiffies(8));
 
-    /* Initialize and start the 1 second timer */
+    // /* Initialize and start the 1 second timer */
     timer_setup(&timer_1s, timer_1s_callback, 0);
     mod_timer(&timer_1s, jiffies + msecs_to_jiffies(1000));
 
-    /* Initialize and start the 400 us timer */
-    timer_setup(&timer_400us, timer_400us_callback, 0);
-    mod_timer(&timer_400us, jiffies + usecs_to_jiffies(400));
+    // /* Initialize and start the 400 us timer */
+    // timer_setup(&timer_400us, timer_400us_callback, 0);
+    // mod_timer(&timer_400us, jiffies + usecs_to_jiffies(400));
 
-    /* Initialize and start the 4 us timer */
-    timer_setup(&timer_4us, timer_4us_callback, 0);
-    mod_timer(&timer_4us, jiffies + usecs_to_jiffies(4));
+    // /* Initialize and start the 4 us timer */
+    // timer_setup(&timer_4us, timer_4us_callback, 0);
+    // mod_timer(&timer_4us, jiffies + usecs_to_jiffies(4));
 
     return 0;
 }
@@ -253,10 +276,10 @@ static void __exit ModuleExit(void)
     gpio_free(GPIO_13);
 
     /* Stop and free the timers */
-    del_timer(&timer_8ms);
+    // del_timer(&timer_8ms);
     del_timer(&timer_1s);
-    del_timer(&timer_400us);
-    del_timer(&timer_4us);
+    // del_timer(&timer_400us);
+    // del_timer(&timer_4us);
 }
 
 module_init(ModuleInit);
